73dd3d32f5b83b614b6b05dc5ba681c5a1dee224
BuildTools <unconfigured@null.spigotmc.org>
2021-01-02 23:42:39 +0900

    Add Example

---
+++ YourWallet.cs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace YourWallet.cs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool useSecure = false;
+            while (true)
+            {
+                Console.WriteLine("Use secure mode ? (y/n)");
+                var modeInput = Console.ReadLine();
+                if(modeInput.Trim().ToLower() == "y")
+                {
+                    useSecure = true;
+                    break;
+                }else if (modeInput.Trim().ToLower() == "n")
+                {
+                    useSecure = false;
+                    break;
+                }
+            }
+
+            if (useSecure)
+            {
+                var wallet = new Wallet_s();
+                while (true)
+                {
+                    Console.WriteLine($"You have {wallet.Money.ToPrimitive()} JPY");
+                    Console.WriteLine($"-- You will pay 10 JPY. Press any key --");
+                    Console.ReadKey();
+                    wallet.Money = wallet.Money -= 10;
+                }
+            }
+            else
+            {
+                var wallet = new Wallet();
+                while (true)
+                {
+                    Console.WriteLine($"You have {wallet.Money} JPY");
+                    Console.WriteLine($"-- You will pay 10 JPY. Press any key --");
+                    Console.ReadKey();
+                    wallet.Money = wallet.Money -= 10;
+                }
+            }
+        }
+    }
+}
---
+++ YourWallet.cs/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using SecureNumber.Types;
+
+namespace YourWallet.cs
+{
+    public class Wallet_s
+    {
+        public UInt32_s Money { get; set; } = 5000;
+    }
+
+    public class Wallet
+    {
+        public uint Money { get; set; } = 5000;
+    }
+}
